b9e79d83c78b783b6d5419029a20f745430714b0
kondors1995 <normandija1945@gmail.com>
2021-11-07 06:37:21 +0000

    laurel_sprout: Add Livedisplay 2.1

--- device.mk
+++ device.mk
@@ -287,7 +287,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.1-service.laurel_sprout
 
 # Media
 PRODUCT_COPY_FILES += \
