1594102da8cfef04c1a2e5a13910d514768b781e
PowerX-NOT <pugalg100@gmail.com>
2023-01-18 19:13:40 +0530

    crdroid: A13

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -19,17 +19,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common crdroid stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 SKIP_ABI_CHECKS := true
-TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Inherit MindTheGapps
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
@@ -42,3 +38,20 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := laurel_sprout
 
 BUILD_FINGERPRINT := Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.26.0.RFQMIXM:user/release-keys
+
+# FOD Animation
+EXTRA_UDFPS_ANIMATIONS := true
+EXTRA_FOD_ANIMATIONS := true
+HAS_FOD := true
+
+# Props
+WITH_GMS := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Pixel charging animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
