55af0c4002f8fce607cc65021ccdaff6429a3270
PowerX-NOT <pugalg100@gmail.com>
2023-03-05 09:41:38 +0530

    Revert "laurel_sprout: Implement SunlightEnhancement LiveDisplay HAL"
    This reverts commit 30b7448ffe6035b8aef4a0dc14d5296ec557f733.

--- device.mk
+++ device.mk
@@ -287,8 +287,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-		vendor.lineage.livedisplay@2.0-service.xiaomi_laurel_sprout
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
